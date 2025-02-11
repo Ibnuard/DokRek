a0040fd6c0c889f4fd959f6868b55b4655e3a1c3
ibnuputraard <ibnu.putra@nawadata.id>
2025-02-11 14:07:55 +0700

    update layout to white theme

--- src/components/CardAccount.tsx
+++ src/components/CardAccount.tsx
@@ -3,17 +3,21 @@ import DotBg from "../assets/dot-bg.svg";
 
 function CardAccount() {
   return (
-    <div className="relative border p-4 rounded-md flex flex-row items-center justify-between transition-color border-slate-400 hover:border-gray-800 overflow-hidden">
+    <div className="relative border p-4 rounded-md flex items-center justify-between transition-colors border-slate-400 hover:border-gray-800 overflow-hidden">
       {/* Background Pattern */}
       <div className="absolute inset-0 w-full h-full bg-[radial-gradient(circle,#00000020_1px,transparent_2px)] bg-[size:16px_16px] z-[-1]"></div>
 
-      {/* Konten */}
-      <div className="flex flex-col gap-2">
+      {/* Konten Kiri */}
+      <div className="flex flex-col gap-2 flex-1 min-w-0">
         <p className="text-gray-500 text-sm font-medium">MANDIRI</p>
-        <p className="text-2xl font-medium text-gray-950">123457</p>
-        <p className=" text-sm text-gray-700 mt-2">Mr. What sit Toya</p>
+        <p className="text-2xl font-medium text-gray-950 max-w-full break-all">
+          12345789
+        </p>
+        <p className="text-sm text-gray-700 mt-2">Mr. What sit Toya</p>
       </div>
-      <div className="flex flex-row gap-4">
+
+      {/* Ikon Kanan */}
+      <div className="flex flex-row gap-4 shrink-0 ml-4">
         <ClipboardIcon className="w-6 h-6 cursor-pointer" />
         <PencilIcon className="w-6 h-6 cursor-pointer" />
       </div>
--- src/components/Modal/InputModal.tsx
+++ src/components/Modal/InputModal.tsx
@@ -8,52 +8,50 @@ export function InputModal() {
 
   return (
     <dialog id="input_modal" className="modal">
-      <div className="modal-box">
-        <h3 className="text-lg font-semibold text-white">Tambahkan Akun</h3>
+      <div className="modal-box bg-white">
+        <h3 className="text-lg font-semibold text-black">Tambahkan Akun</h3>
         <div className="flex flex-col gap-4 mt-8">
           <label className="form-control w-full max-w-lg">
             <div className="label mb-3">
-              <span className="label-text text-slate-200">
+              <span className="label-text text-slate-800">
                 Nomor rekening / akun
               </span>
             </div>
             <input
               type="text"
               placeholder="Masukan nomor rekening / akun"
-              className="input input-md input-bordered w-full max-w-lg text-white"
+              className="input input-md input-bordered w-full max-w-lg text-black border border-slate-500 bg-white placeholder:text-slate-400"
             />
           </label>
           <label className="form-control w-full max-w-lg">
             <div className="label mb-3">
-              <span className="label-text text-slate-200">
-                Nomor rekening / akun
+              <span className="label-text text-slate-800">
+                Nomor rekening atau nomor akun
               </span>
             </div>
             <input
               type="text"
               placeholder="Masukan nomor rekening / akun"
-              className="input input-md input-bordered w-full max-w-lg text-white"
+              className="input input-md input-bordered w-full max-w-lg text-black border border-slate-500 bg-white placeholder:text-slate-400"
             />
           </label>
           <label className="form-control w-full max-w-lg">
             <div className="label mb-3">
-              <span className="label-text text-slate-200">
-                Nama Pemilik Rekening
+              <span className="label-text text-slate-800">
+                Nama pemilik rekening
               </span>
             </div>
             <input
               type="text"
               placeholder="Masukan nomor rekening / akun"
-              className="input input-md input-bordered w-full max-w-lg text-white"
+              className="input input-md input-bordered w-full max-w-lg text-black border border-slate-500 bg-white placeholder:text-slate-400"
             />
           </label>
         </div>
         <div className="flex flex-row items-center justify-center gap-4 mt-8">
-          <button className="btn btn-primary font-normal text-white">
-            Simpan
-          </button>
+          <button className="btn font-normal text-white">Simpan</button>
           <button
-            className="btn btn-outline font-normal text-white"
+            className="btn btn-outline font-normal text-black hover:bg-white"
             onClick={onCancelButton}
           >
             Batalkan
